903102ae3256da2651f0e1da318d8dfc494d3a2d
Julien <jbrochier@ensc.fr>
2020-05-04 14:26:30 +0200

    generate N images

--- image_generator.py
+++ image_generator.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.patches as mpatches
 import random
 import os
+import sys
 
 def create_circle(loc,h,color):
     circle = mpatches.Circle(loc, h/2, color=color)
@@ -24,33 +25,38 @@ def create_wedge(loc,h,color):
     wedge = mpatches.Wedge(loc, h/2, 0, 270, color=color)
     return wedge
 
-shapes = [create_circle,create_rectangle,create_polygon,create_ellipse,create_wedge]
 
-dataset=[]
-DATASET_WIDTH = 20
-IMAGE_WIDTH = 256
-plt.rcParams['figure.facecolor'] = 'black'
-DIR_PATH = './data'
+def generate_images(dataset_width=100):
+    shapes = [create_circle,create_rectangle,create_polygon,create_ellipse,create_wedge]
 
-for i in range(DATASET_WIDTH):
-    nb_shapes = random.randint(3,5)
-    patches = []
+    IMAGE_WIDTH = 256
+    plt.rcParams['figure.facecolor'] = 'black'
+    DIR_PATH = './data'
 
-    ax = plt.gca()
-    ax.set_xlim(0,IMAGE_WIDTH)
-    ax.set_ylim(0,IMAGE_WIDTH)
-    plt.axis('off')
+    for i in range(dataset_width):
+        nb_shapes = random.randint(3,5)
 
-    for j in range (nb_shapes):
-        loc = [random.randint(int(round(IMAGE_WIDTH/8)),int(round(IMAGE_WIDTH*7/8))),random.randint(int(round(IMAGE_WIDTH/8)),int(round(IMAGE_WIDTH*7/8)))]
-        height = (1+random.random())*IMAGE_WIDTH/8
-        color = str(np.random.randint(70,255)/255)
-        shape = random.choice(shapes)(loc,height,color)
-        ax.add_patch(shape)
-    file_name = "img{:d}.png".format(i)
-    file_path = os.path.join(DIR_PATH,file_name)
-    plt.savefig(file_path,dpi=100, facecolor='black', bbox_inches = 'tight', pad_inches = 0.)
-    #print(nb_shapes)
-    #plt.show()
-    plt.clf()
+        ax = plt.gca()
+        ax.set_xlim(0,IMAGE_WIDTH)
+        ax.set_ylim(0,IMAGE_WIDTH)
+        plt.axis('off')
 
+        for j in range (nb_shapes):
+            loc = [random.randint(int(round(IMAGE_WIDTH/8)),int(round(IMAGE_WIDTH*7/8))),random.randint(int(round(IMAGE_WIDTH/8)),int(round(IMAGE_WIDTH*7/8)))]
+            height = (1+random.random())*IMAGE_WIDTH/8
+            color = str(np.random.randint(70,255)/255)
+            shape = random.choice(shapes)(loc,height,color)
+            ax.add_patch(shape)
+        file_name = "img{:d}.png".format(i)
+        file_path = os.path.join(DIR_PATH,file_name)
+        plt.savefig(file_path,dpi=100, facecolor='black', bbox_inches = 'tight', pad_inches = 0.)
+        #print(nb_shapes)
+        #plt.show()
+        plt.clf()
+
+if __name__ == "__main__":
+    if(len(sys.argv)>1):
+        dataset_width = int(sys.argv[1])
+        generate_images(dataset_width)
+    else:
+        generate_images(100)
